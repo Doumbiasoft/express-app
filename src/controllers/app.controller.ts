926d3fd8bcf044eb1f003e1e2828bc70124f80fa
Mouhamed Doumbia <doumbiasoft@gmail.com>
2025-09-03 22:28:41 -0400

    updated the app controller

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -3,23 +3,21 @@ import { postData, renderHome } from "../services/appService";
 import { sendResponse } from "../utils/response";
 
 class AppController {
-  getHome = async (req: Request, res: Response) => {
+  async getHome(req: Request, res: Response) {
     const { firstName } = req.params;
     return res.render(
       "home",
       renderHome("Hey", `${firstName} how are you doing?`)
     );
-  };
-
-  getForm = async (req: Request, res: Response) => {
+  }
+  async getForm(req: Request, res: Response) {
     return res.render("form");
-  };
-  postForm = async (req: Request, res: Response) => {
+  }
+  async postForm(req: Request, res: Response) {
     const { name } = req.body;
     console.log(name);
-
     return sendResponse(res, postData(name));
-  };
+  }
 }
 const appController = new AppController();
 export const getHome = appController.getHome;
